ff6d0c53de3a2d875142c6ac6ca0462ea85f4e00
Quincy Fer <quincy.e.fer@gmail.com>
2025-01-17 10:58:45 +0100

    delete css file, setup title

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import "./App.css";
-
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold">
-      Hello world!
-    </h1>
-  )
+    <>
+      <h1 className="text-3xl font-bold ">Hello world!</h1>
+    </>
+  );
 }
-
-
