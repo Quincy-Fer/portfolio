fc2a29522d809f28491752c516fb911aea8f291e
Quincy Fer <quincy.e.fer@gmail.com>
2025-01-21 14:00:14 +0100

    Rewritten text and added emoticons

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,10 +8,10 @@ const Hero = () => {
         <br />
         Een frontend developer |
       </h1>
-      <p className="mt-12 max-w-[520px] text-xl">
+      <p className="mt-12 max-w-[520px] text-xl leading-relaxed tracking-wide ">
         Ik ben een gepassioneerde webontwikkelaar gespecialiseerd in het maken
-        van visueel aantrekkelijke websites. Ik hou van het oplossen van
-        problemen en met behulp van code creatieve ideeën tot leven brengen!
+        van 🎨 visueel aantrekkelijke websites. Ik hou van het oplossen van
+        problemen 🧩 en met behulp van code 👨‍💻 creatieve  ideeën tot leven brengen!
       </p>
     </section>
   );
