7cfe3048f6e2103fc6e67d10771c9f5d1624a27d
Quincy Fer <quincy.e.fer@gmail.com>
2025-01-23 15:14:03 +0100

    Formatting layout making minor changes to styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,15 +5,20 @@ import ButtonBorder from "./ButtonBorder";
 const Hero = () => {
   return (
     <section className="p-48  background-pattern  ">
-      <h1 className="bg-white max-w-[600px] font-montserrat font-bold  text-6xl text-gray-800 
-      leading-tight tracking-tighter">
+      <h1
+        className="bg-white max-w-[600px] font-montserrat font-bold  text-6xl text-gray-800 
+      leading-tight tracking-tighter"
+      >
         Hoi, ik ben <span className="text-teal-400"> Quincy.</span>
         <br />
         Frontend developer |
       </h1>
-      <p className="bg-white mt-10 max-w-[520px] font-barlow text-xl leading-relaxed tracking-wide ">
+      <p
+        className="bg-white mt-10 max-w-[520px] font-barlow text-xl leading-relaxed
+       tracking-wide text-gray-700 "
+      >
         Ik ben een gepassioneerde webontwikkelaar gespecialiseerd in het maken
-        van 🎨 visueel aantrekkelijke websites. Ik hou van het oplossen van
+        van  visueel 🎨 aantrekkelijke websites. Ik hou van het oplossen van
         problemen 🧩 en met behulp van code 👨‍💻 creatieve ideeën tot leven
         brengen!
       </p>
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -3,25 +3,37 @@ import React from "react";
 const Skills = () => {
   return (
     <section className="bg-gradient-to-r from-teal-300 via-teal-400 to-cyan-300 css-cut ">
-      <h2 className="text-center font-montserrat font-bold  text-4xl text-white py-10 ">
+      <h2 className="text-center font-montserrat font-bold  text-5xl text-white py-10 ">
         Vaardigheden
       </h2>
-      <div className="flex flex-col gap-8 px-48 py-10  ">
+      <div className=" pb-20 pt-14 pl-48  ">
         <div className="flex">
-          <p className="text-white text-xl font-barlow font-bold">
+          <p className="min-w-[400px] text-white font-barlow font-bold text-2xl pb-10">
             Programmeertalen:
           </p>
-          <p>lalaaaaaaa</p>
+          <div >
+            <p>lalalal</p>
+            <p>lalalal</p>
+            
+          </div>
         </div>
         <div className="flex">
-          <p className="text-white text-xl font-barlow font-bold">
-            Libraries & Frameworks:
+          <p className="min-w-[400px] text-white font-barlow font-bold text-2xl pb-10">
+            Libraries en Frameworks:
           </p>
-          <p>lalala</p>
+          <div>
+            <p>lalalal</p>
+            <p>lalalal</p>
+          </div>
         </div>
-        <div className="flex mb-40">
-          <p className="text-white text-xl font-barlow font-bold">Tools:</p>
-          <p>lalalaaaaa</p>
+        <div className="flex">
+          <p className="min-w-[400px] text-white font-barlow font-bold text-2xl pb-10">
+            Tools:
+          </p>
+          <div>
+            <p>lalalal</p>
+            <p>lalalal</p>
+          </div>
         </div>
       </div>
     </section>
