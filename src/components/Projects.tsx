20927181a80c6b8166a88df902fa8a8b24a6f282
Quincy Fer <quincy.e.fer@gmail.com>
2025-01-30 19:01:07 +0100

    create custom box shadow, add styling and content for project

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import pulsefit from '../assets/images/pulse-fit.png'
+import pulsefit from "../assets/images/pulse-fit.png";
 
 const Projects = () => {
   return (
@@ -8,25 +8,39 @@ const Projects = () => {
         Projecten
       </h2>
       {/* project 1 */}
-      <div className="grid grid-cols-2 h-[360px]">
-        <div>
-          <h4>lalalalalalalalalala</h4>
-          <p>lala</p>
-          <h4>lalalalalalalalalala</h4>
-          <h4>lalalalalalalalalala</h4>
-          <h4>lalalalalalalalalala</h4>
-          <h4>lalalalalalalalalala</h4>
-          <h4>lalalalalalalalalala</h4>
-          <h4>lalalalalalalalalala</h4>
-          <h4>lalalalalalalalalala</h4>
-          <h4>lalalalalalalalalala</h4>
-          <h4>lalalalalalalalalala</h4>
-          <h4>lalalalalalalalalala</h4>
-          
+      <div className="grid grid-cols-2 h-[360px] mt-16">
+        {/* informatie project */}
+        <div className="">
+          <h4 className="text-gray-800 font-bold uppercase text-3xl font-montserrat">
+            Pulse-Fit
+          </h4>
+          <div className="flex py-5 gap-4">
+            <div className="bg-teal-700 px-4 py-2 font-semibold text-white font-barlow uppercase text-lg">
+              Next.js
+            </div>
+            <div className="bg-teal-700 px-4 py-2 font-semibold text-white font-barlow uppercase text-lg">
+              React
+            </div>
+            <div className="bg-teal-700 px-4 py-2 font-semibold text-white font-barlow uppercase text-lg">
+              Typescript
+            </div>
+            <div className="bg-teal-700 px-4 py-2 font-semibold text-white font-barlow uppercase text-lg">
+              Tailwind
+            </div>
+          </div>
+          <p>omschrijving</p>
+
+          <div className="flex">
+            <button>live website</button>
+            <button>Github</button>
+          </div>
         </div>
-        <img src={pulsefit} alt="" className="w-full h-full" />
-     
-        
+        {/* afbeelding */}
+        <img
+          src={pulsefit}
+          alt="image for gym website project"
+          className="w-full h-full shadow-md  box-shadow"
+        />
       </div>
     </section>
   );
