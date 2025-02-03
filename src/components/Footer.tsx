1935bcb2cc3545eadb5fc31ff9e6cce8c952e76c
Quincy Fer <quincy.e.fer@gmail.com>
2025-02-03 22:19:42 +0100

    Added footer and colored scrollwheel

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="bg-teal-400 text-white text-center py-4">
+      <p className="text-sm text-white font-bold">
+        &copy; {new Date().getFullYear()} Quincy Fer. All rights reserved.
+      </p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
