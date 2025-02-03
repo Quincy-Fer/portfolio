6035cb638e7cf32f6aaa299d6b36a75af2500036
Quincy Fer <quincy.e.fer@gmail.com>
2025-02-03 22:05:50 +0100

    Created contact section and contactbutton

---
+++ src/components/ContactButton.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ButtonBorder = () => {
+  return (
+    <button
+      type="button"
+      className="border-teal-400 bg-white  text-xl font-bold text-teal-400 border-4 px-16 py-4 transition-all  hover:bg-teal-400
+       hover:text-white"
+    >
+      Stuur Berichtje
+    </button>
+  );
+};
+
+export default ButtonBorder;
