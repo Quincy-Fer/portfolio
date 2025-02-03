c5faf9d780186c337b6cd2ef3b1a2f56bf0e8c37
Quincy Fer <quincy.e.fer@gmail.com>
2025-02-03 22:33:39 +0100

    Add footer, add react-icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <footer className="bg-teal-400 text-white text-center py-4">
-      <p className="text-sm text-white font-bold">
-        &copy; {new Date().getFullYear()} Quincy Fer. All rights reserved.
+    <footer className="bg-teal-400 text-white  py-4 pb-10">
+      <div className="flex  justify-center gap-4 mt-4 mb-4">
+        <p className="font-barlow text-lg font-bold tracking-widest ">Github en LinkedIn: </p>
+        <a
+          href="https://github.com/Quincy-Fer"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-teal-700 transition"
+        >
+          <FaGithub size={40} />
+        </a>
+        <a
+          href="https://linkedin.com/in/yourlinkedin"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-teal-700 transition"
+        >
+          <FaLinkedin size={40} />
+        </a>
+      </div>
+      <p className="text-md font-barlow font-bold text-end mr-4">
+        &copy; {new Date().getFullYear()} Copyright Quincy Fer
       </p>
     </footer>
   );
